81b0f3c19693dd134cac805844bfea7597e55d3c
Valerio Di Zio <diziovalerio@gmail.com>
2024-09-25 11:43:40 +0200

    refactor(group): updated grpc implementation of group, in line with user update, using grpc-kotlin

--- domain/src/main/kotlin/User.kt
+++ domain/src/main/kotlin/User.kt
@@ -15,4 +15,18 @@ data class User(
     val email: String,
     val password: String,
     val role: String,
-)
+) {
+    /**
+     * Companion object for the User class.
+     */
+    companion object {
+        /**
+         * Returns an empty User instance with all properties set to empty strings.
+         *
+         * @return an empty User instance
+         */
+        fun getEmptyUser(): User {
+            return User("", "", "", "", "", "")
+        }
+    }
+}
