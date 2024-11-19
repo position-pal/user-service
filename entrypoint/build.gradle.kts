02d8984db62dffc037b8e75e61df950049da8f58
Valerio Di Zio <diziovalerio@gmail.com>
2024-11-19 17:11:58 +0100

    feat: simple entrypoint

--- entrypoint/build.gradle.kts
+++ entrypoint/build.gradle.kts
@@ -1,4 +1,21 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("MainKt") // Sostituisci con la tua classe principale
+}
+
 dependencies {
+    implementation(libs.grpc.stub)
+    implementation(libs.grpc.netty.shaded)
+    implementation(libs.ktorm.core)
     api(project(":storage"))
     api(project(":grpc"))
+    api(project(":rabbitmq"))
+}
+
+tasks.named("run") {
+    dependsOn(":composeUp")
+    finalizedBy(":composeDown")
 }
--- entrypoint/src/main/kotlin/Main.kt
+++ entrypoint/src/main/kotlin/Main.kt
@@ -1,6 +1,60 @@
+import auth.AuthServiceImpl
+import group.GroupServiceImpl
+import io.github.positionpal.AvroSerializer
+import io.grpc.Server
+import io.grpc.ServerBuilder
+import user.UserServiceImpl
+
 /**
- * This file will contain the application entrypoint.
+ * The port number for the gRPC server.
+ */
+const val PORT = 8080
+
+/**
+ * The main entry point of the application.
+ * This function sets up and starts the gRPC server with the necessary service adapters.
  */
 fun main() {
-    println("Hello World!")
+    // Initialize the message adapter with Avro serialization
+    val messageAdapter = RabbitMQMessageAdapter(serializer = AvroSerializer())
+
+    // Initialize the authentication service adapter with the necessary dependencies
+    // TODO: Replace the placeholder secret with the actual secret
+    val authAdapter = GrpcAuthServiceAdapter(
+        AuthServiceImpl(
+            PostgresAuthRepository(),
+            Secret("secretsGoHere"),
+            Issuer("io.github.positionpal"),
+            Audience("positionpal.io"),
+        ),
+    )
+
+    // Initialize the group service adapter with the necessary dependencies
+    val groupAdapter = GrpcGroupServiceAdapter(
+        GroupServiceImpl(
+            PostgresGroupRepository(),
+            messageAdapter,
+        ),
+    )
+
+    // Initialize the user service adapter with the necessary dependencies
+    val userAdapter = GrpcUserServiceAdapter(
+        UserServiceImpl(
+            PostgresUserRepository(),
+        ),
+    )
+
+    // Build the gRPC server and add the service adapters
+    val server: Server = ServerBuilder.forPort(PORT)
+        .addService(authAdapter)
+        .addService(groupAdapter)
+        .addService(userAdapter)
+        .build()
+
+    // Start the server
+    server.start()
+    println("Server started on port 8080")
+
+    // Keep the server running
+    server.awaitTermination()
 }
